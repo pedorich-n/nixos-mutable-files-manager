3a47c1f35def4d7c8e1cbd704c790da1d768f555
Nikita Pedorich <pedorich.n@gmail.com>
2023-08-09 23:50:21 +0900

    Basic implementation added

---
+++ src/manage_files.py
@@ -0,0 +1,161 @@
+from argparse import ArgumentParser
+import logging
+import os
+import shutil
+from pathlib import PosixPath
+from typing import Callable, Iterable, List, Optional, TypeVar
+
+T = TypeVar("T")
+
+logger = logging.getLogger("manage_files")
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="[{asctime}] [{levelname:<5s}] [{name}:{lineno:03}] - {message}",
+    datefmt="%Y-%m-%dT%H:%M:%S%z",
+    style="{",
+)
+
+
+def _dry_run_wrap(callable: Callable[[], T], dry_run: bool) -> Optional[T]:
+    if dry_run:
+        return None
+    else:
+        return callable()
+
+
+def convert_paths_to_relative(root: PosixPath, paths: Iterable[PosixPath]) -> List[PosixPath]:
+    return [path.relative_to(root) for path in paths]
+
+
+# region "State"
+def read_state_or_empty(path: PosixPath) -> List[PosixPath]:
+    if path.exists():
+        with open(path, "r") as file:
+            return [PosixPath(path) for path in file.read().splitlines()]
+    else:
+        return []
+
+
+def write_state(path: PosixPath, state: Iterable[PosixPath], dry_run: bool) -> None:
+    def inner():
+        with open(path, "w") as file:
+            for line in state:
+                file.write(f"{line}\n")
+
+    return _dry_run_wrap(inner, dry_run)
+
+
+def get_files_to_delete_from_states(old: Iterable[PosixPath], new: Iterable[PosixPath]) -> List[PosixPath]:
+    old_set = set(old)
+    new_set = set(new)
+
+    result_set = old_set.difference(new_set)
+
+    result = list(result_set)
+    result.sort()
+
+    return result
+
+
+# endregion
+
+
+# region Files
+def get_rel_files_recursively(
+    root: PosixPath,
+) -> List[PosixPath]:
+    files = []
+    for dirpath, _, filenames in os.walk(root, followlinks=False):
+        for filename in filenames:
+            file_path = PosixPath(dirpath).joinpath(filename)
+            rel_path = file_path.relative_to(root)
+            files.append(rel_path)
+    return files
+
+
+def create_dirs_recursively(
+    target_root: PosixPath,
+    relative_source_paths: Iterable[PosixPath],
+    dry_run: bool,
+) -> None:
+    for relative_source_path in relative_source_paths:
+        # Assuming here that all paths are file-paths
+        target = target_root.joinpath(relative_source_path.parent)
+
+        logger.info(f"Creating {target}")
+        _dry_run_wrap(
+            lambda: target.mkdir(exist_ok=True, parents=True, mode=0o777),
+            dry_run,
+        )
+
+
+def _copy_file(source: PosixPath, destination: PosixPath, dry_run: bool):
+    prefix = "[DRY_RUN] " if dry_run else ""
+    logger.debug(f"{prefix}Copying file from {source} to {destination}")
+    _dry_run_wrap(
+        lambda: shutil.copyfile(src=source, dst=destination),
+        dry_run,
+    )
+
+
+def copy_files_from_rel(
+    source_root: PosixPath,
+    destination_root: PosixPath,
+    rel_paths: Iterable[PosixPath],
+    dry_run: bool,
+) -> None:
+    for path in rel_paths:
+        source_abs_path = source_root.joinpath(path)
+        destination_abs_path = destination_root.joinpath(path)
+        _copy_file(
+            source=source_abs_path,
+            destination=destination_abs_path,
+            dry_run=dry_run,
+        )
+
+
+def remove_files(paths: Iterable[PosixPath], dry_run: bool):
+    def inner():
+        for path in paths:
+            path.unlink()
+
+    _dry_run_wrap(lambda: inner(), dry_run)
+
+
+# endregion
+
+
+def main():
+    parser = ArgumentParser("")
+    parser.add_argument("--source", type=PosixPath, required=True)
+    parser.add_argument("--destination", type=PosixPath, required=True)
+    # TODO: fix state path
+    parser.add_argument("--state", type=PosixPath, default=PosixPath("/var/lib/test/state"), required=False)
+    parser.add_argument("--dry-run", action="store_true", default=False, required=False)
+    args = parser.parse_args()
+
+    source: PosixPath = args.source
+    destination: PosixPath = args.destination
+    state_path: PosixPath = args.state
+    dry_run: bool = args.dry_run
+
+    source_files = get_rel_files_recursively(source)
+
+    create_dirs_recursively(destination, source_files, dry_run)
+
+    old_state = read_state_or_empty(state_path)
+
+    copy_files_from_rel(source_root=source, destination_root=destination, rel_paths=source_files, dry_run=dry_run)
+
+    # TODO: generate new state from copy_files_from_rel response, once it handles errors
+    new_state = [destination.joinpath(p) for p in source_files]
+    files_to_delete = get_files_to_delete_from_states(old_state, new_state)
+
+    remove_files(files_to_delete, dry_run)
+
+    write_state(state_path, new_state, dry_run)
+
+
+# if __name__ == "__main__":
+#     main()
---
+++ tests/test_manage_files.py
@@ -0,0 +1,146 @@
+from pathlib import PosixPath
+from typing import List
+
+import src.manage_files as manage_files
+
+
+def _assert_file_content(path: PosixPath, content: List[str]):
+    assert path.exists() == True
+    with open(path, "r") as file:
+        lines = file.read().splitlines()
+        assert lines == content
+
+
+# region State
+def test_read_state_or_empty_exists(tmp_path):
+    state = "1.txt\n2.txt\n3.txt\n"
+
+    state_path = tmp_path / "state"
+    state_path.write_text(state)
+
+    result = manage_files.read_state_or_empty(state_path)
+
+    expected = [PosixPath(p) for p in ["1.txt", "2.txt", "3.txt"]]
+    assert result == expected
+
+
+def test_read_state_or_empty_not_exists(tmp_path):
+    state_path = tmp_path / "state"
+
+    result = manage_files.read_state_or_empty(state_path)
+
+    expected = []
+    assert result == expected
+
+
+def test_write_state(tmp_path):
+    state = [PosixPath(p) for p in ["1.txt", "2.txt", "3.txt"]]
+
+    state_path = tmp_path / "state"
+
+    manage_files.write_state(state_path, state, False)
+
+    assert state_path.exists() == True
+    _assert_file_content(state_path, ["1.txt", "2.txt", "3.txt"])
+
+
+def test_get_files_to_delete_from_states():
+    old_state = [PosixPath(p) for p in ["1.txt", "2.txt", "3.txt", "4.txt"]]
+    new_state = [PosixPath(p) for p in ["1.txt", "2.txt", "5.txt"]]
+
+    result = manage_files.get_files_to_delete_from_states(old_state, new_state)
+    expected = [PosixPath(p) for p in ["3.txt", "4.txt"]]
+
+    assert result == expected
+
+
+# endregion
+
+
+# region Files
+def test_get_rel_files_recursively(tmp_path):
+    (tmp_path / "1").mkdir()
+    (tmp_path / "1" / "2.txt").write_text("")
+    (tmp_path / "1" / "3").mkdir()
+    (tmp_path / "1" / "3" / "4.txt").write_text("")
+
+    result = manage_files.get_rel_files_recursively(tmp_path)
+
+    expected = [
+        PosixPath("1/2.txt"),
+        PosixPath("1/3/4.txt"),
+    ]
+
+    assert result == expected
+
+
+def test_create_dirs_recursively(tmp_path):
+    relative_paths = [PosixPath(p) for p in ["1/2.txt", "1/3/4.txt", "5/6/7.txt"]]
+
+    manage_files.create_dirs_recursively(tmp_path, relative_paths, False)
+
+    assert (tmp_path / "1").exists() == True
+    assert (tmp_path / "1" / "3").exists() == True
+    assert (tmp_path / "5" / "6").exists() == True
+
+
+def test__copy_file(tmp_path):
+    (tmp_path / "source").mkdir()
+    source = tmp_path / "source" / "file.txt"
+    source.write_text("test")
+
+    (tmp_path / "destination").mkdir()
+    destination = tmp_path / "destination" / "file.txt"
+
+    assert destination.exists() == False
+
+    manage_files._copy_file(source, destination, False)
+
+    assert destination.exists() == True
+    _assert_file_content(destination, ["test"])
+
+
+def test_copy_files_from_rel(tmp_path):
+    source_folder = tmp_path / "source"
+    source_folder.mkdir(parents=True)
+
+    (source_folder / "1.txt").write_text("one")
+    (source_folder / "2.txt").write_text("two")
+    (source_folder / "3").mkdir()
+    (source_folder / "3" / "4.txt").write_text("four")
+
+    destination_folder = tmp_path / "destination"
+    (destination_folder / "3").mkdir(parents=True)
+
+    relative_paths = [PosixPath(path) for path in ["1.txt", "2.txt", "3/4.txt"]]
+
+    manage_files.copy_files_from_rel(
+        source_root=source_folder,
+        destination_root=destination_folder,
+        rel_paths=relative_paths,
+        dry_run=False,
+    )
+
+    _assert_file_content((destination_folder / "1.txt"), ["one"])
+    _assert_file_content((destination_folder / "2.txt"), ["two"])
+    _assert_file_content((destination_folder / "3" / "4.txt"), ["four"])
+
+
+def test_remove_files(tmp_path):
+    (tmp_path / "1").mkdir()
+    (tmp_path / "1" / "2.txt").write_text("")
+    (tmp_path / "1" / "3").mkdir()
+    (tmp_path / "1" / "3" / "4.txt").write_text("")
+
+    paths = [tmp_path.joinpath(PosixPath(p)) for p in ["1/2.txt", "1/3/4.txt"]]
+
+    assert (tmp_path / "1" / "2.txt").exists() == True
+    assert (tmp_path / "1" / "3" / "4.txt").exists() == True
+
+    manage_files.remove_files(paths, False)
+
+    assert (tmp_path / "1" / "2.txt").exists() == False
+    assert (tmp_path / "1" / "3" / "4.txt").exists() == False
+
+
+# endregion
