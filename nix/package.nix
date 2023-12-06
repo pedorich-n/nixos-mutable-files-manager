ec26acd7013bd8526f8f9813c0da487802f60774
Nikita Pedorich <pedorich.n@gmail.com>
2023-12-06 12:39:04 +0900

    Add meta.mainProgram

--- nix/package.nix
+++ nix/package.nix
@@ -2,6 +2,7 @@
 pkgs.poetry2nix.mkPoetryApplication {
   projectDir = ./..;
   checkGroups = [ ]; # To omit dev dependencies
+  meta.mainProgram = "nixos-mutable-files-manager";
   overrides = pkgs.poetry2nix.overrides.withDefaults (_: prev: {
     expression = prev.expression.overridePythonAttrs (old: { buildInputs = (old.buildInputs or [ ]) ++ [ prev.poetry-core ]; });
   });
